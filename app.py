0a6a62b57358d0e823fc7e83b25ba5243fb00377
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-07-13 16:34:58 -0400

    code

--- app.py
+++ app.py
@@ -83,6 +83,28 @@ class Client:
     
     all=[]
 
+    @classmethod
+    def drop_table(cls):
+        query="drop table if exists clients;"
+        conn=sqlite3.connect(DATABASE_URL)
+        cursor=conn.cursor()
+        cursor.execute(query)
+
+    @classmethod
+    def create_table(cls):
+        query="""
+        CREATE TABLE if not exists clients(
+            id integer primary key,
+            fname text not null,
+            lname text not null,
+            phone text not null,
+            email text not null
+            );
+        """
+        conn=sqlite3.connect(DATABASE_URL)
+        cursor=conn.cursor()
+        cursor.execute(query)
+
     def __init__(self,fname,lname,phone,email):
         self.fname=fname
         self.lname=lname
@@ -90,8 +112,20 @@ class Client:
         self.email=email
         self.all.append(self)
 
+    def save(self):
+        Client.create_table()
+        query="""
+                INSERT INTO clients(fname,lname,phone,email) values(?,?,?,?);
+                """
+        conn=sqlite3.connect(DATABASE_URL)
+        cursor=conn.cursor()
+        result=cursor.execute(query,(self.fname,self.lname,self.phone,self.email))
+        conn.commit()
+        self.id=result.lastrowid
+
     def book_appointment(self,barber,date,cut_style,price):
         pass
+
     @classmethod
     def get_client_by_id(cls,id):
         pass
@@ -102,6 +136,31 @@ class Client:
 class Appointment:
 
     all=[]
+
+    @classmethod
+    def drop_table(cls):
+        query="drop table if exists appointments;"
+        conn=sqlite3.connect(DATABASE_URL)
+        cursor=conn.cursor()
+        cursor.execute(query)
+
+    @classmethod
+    def create_table(cls):
+        query="""
+        CREATE TABLE if not exists appointments(
+            id integer primary key,
+            client_id integer not null,
+            barber_id integer not null,
+            date text not null,
+            cut_style text,
+            price real not null,
+            foreign key(client_id) references clients(id),
+            foreign key(barber_id) references barbers(id)
+            );
+        """
+        conn=sqlite3.connect(DATABASE_URL)
+        cursor=conn.cursor()
+        cursor.execute(query)
     
     def __init__(self,client_id,barber_id,date,cut_style,price):
         self.client_id=client_id
@@ -119,8 +178,16 @@ Barber.drop_table()
 manuel=Barber("Manuel","Garcia","07-12-2023","779-343-3434")
 manuel.save()
 print(manuel)
-# bob=Client("Bob","Miller","343-343-3434","millerb@gmail.com")
-# steve=Client("Steve","Smith","993-333-4343","smiths@gmail.com")
+
+print()
+
+Client.drop_table()
+bob=Client("Bob","Miller","343-343-3434","millerb@gmail.com")
+bob.save()
+print(bob)
+steve=Client("Steve","Smith","993-333-4343","smiths@gmail.com")
+steve.save()
+print(steve)
 
 # bob.book_appointment(manuel,"07-25-2023","spike",20)
 # bob.book_appointment(manuel,"07-30-2023","regular",7)
